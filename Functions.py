00f625a6023a9dcaa2e2f171d2a878b53baf7afb
Radosław Radzio <radeczkowo@gmail.com>
2022-03-08 00:49:17 +0100

    V1.Combined categorical values into new one in variables which have values with very little share in variable.

--- Functions.py
+++ Functions.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -16,14 +17,32 @@ def plotobservationpercentage(obs_percent, variable):
     plt.show()
 
 
-
-def observationpercentagetodrop(df_train, variable, threshold):
+def calculateobspercentage(df_train, variable):
     obs_count = df_train[f'{variable}'].value_counts()
     obs_percent = obs_count.apply(lambda x: x/obs_count.sum()).sort_values(ascending=False)
-    #plotobservationpercentage(obs_percent, variable)
-    print(obs_percent[0])
+    print(obs_percent)
+    plotobservationpercentage(obs_percent, variable)
+    return obs_percent
+
+
+
+def observationpercentagetodrop(df_train, variable, threshold):
+    obs_percent = calculateobspercentage(df_train, variable)
+    #print(obs_percent[0])
     if obs_percent.values[0] > threshold:
-        print("To drop")
+        #print("To drop")
         return False
-    print("Not to drop")
+    #print("Not to drop")
     return True
+
+def boxplotxy(df_train, x, y):
+    Xy_group = df_train.groupby(x)[y].mean().sort_values(ascending=False)
+    print(Xy_group)
+    Xy_group.plot(kind="bar", rot=0, xlabel=x, ylabel=f"Average {y}", title=f"Average {y} by {x}")
+    plt.show()
+
+
+def replaceobserwations(df_train, df_test, column, toreplace, replacement):
+    df_train[column] = df_train[column].replace(toreplace, replacement)
+    df_test[column] = df_test[column].replace(toreplace, replacement)
+
--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 from Functions import *
-import numpy as np
-from sklearn import preprocessing
-import sklearn
-from sklearn.ensemble import ExtraTreesClassifier
 
 pd.set_option('display.max_columns', 80)
 pd.set_option('display.max_rows', 100)
@@ -13,8 +9,8 @@ df_test = pd.read_csv("Data/test.csv", index_col=0)
 
 # Data Understanding
 
-print(df_train.info())
-print(len(df_train.select_dtypes('object').columns))
+#print(df_train.info())
+# print(len(df_train.select_dtypes('object').columns))
 
 '''
 80 columns
@@ -25,9 +21,9 @@ print(len(df_train.select_dtypes('object').columns))
 # Drop columns in which percentage of missing values  is greater than threshold given.
 df_train, df_test = dropcolumnstesttrain(df_train, df_test, getcolumnswithdatamissing(df_train, 0.7))
 
-# Categorical Variables
+# Categorical Variables pre-cleaning
 
-print(df_train.select_dtypes('object').info())
+# print(df_train.select_dtypes('object').info())
 
 # Finding columns in which percentage of the share of one variable is greater than threshold given.
 columns = []
@@ -35,15 +31,18 @@ for variable in df_train.select_dtypes('object').columns:
     if not observationpercentagetodrop(df_train.select_dtypes('object'), variable, 0.75):
         columns.append(variable)
 
-print(columns)
+# print(columns)
 
 # Dropping columns found before
 df_train, df_test = dropcolumnstesttrain(df_train, df_test, columns)
-print(df_train.select_dtypes('object').info())
 
-#print(tcn_percent)
-#print(df_train_cat.head(10))
-#print(df_train_cat.nunique())
+# Saving pre-cleaned data
+df_train.to_csv('Data/train_V1.csv', index=False)
+df_test.to_csv('Data/test_V1.csv', index=False)
+
+
+
+
 
 
 
